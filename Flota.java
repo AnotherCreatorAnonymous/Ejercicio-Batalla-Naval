1a06b2a2288ffdbf4bdf82111900738016ca3b60
Defender21 <carlospipe21@gmail.com>
2026-08-15 14:58:25 -0500

    Inicio de ejercicio

---
+++ Flota.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Flota {
+
+    public boolean esBuenAtaque(int longitud, int latitud){
+        return true;
+    }
+
+    public boolean problemaEnAire(){
+        return true;
+    }
+
+    public ArrayList<Object> seranDestruidos(int longitud, int latitude){
+        return ArrayList<>;
+    }
+}
