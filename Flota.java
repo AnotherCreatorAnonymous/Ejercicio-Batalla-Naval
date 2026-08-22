e468842efa4e9d8d73b9621dbf3d1e24c4d71f42
Defender21 <carlospipe21@gmail.com>
2026-08-22 17:01:01 -0500

    Metodo esBuenAtaque y mini comentarios de cada metodo

--- Flota.java
+++ Flota.java
@@ -2,15 +2,26 @@ import java.util.ArrayList;
 
 public class Flota {
 
+    private ArrayList<Barco> barcos;
+
+
+    // Consulta en la lista de Barcos(o unidades) si hay un barco aliado en la posición (longitud, latitud) 
     public boolean esBuenAtaque(int longitud, int latitud){
+        for(Barco barco : barcos ){
+            if(longitud == barco.getLongitud() && latitud == barco.getLatitud()){
+                return false;
+            }
+        }
         return true;
     }
 
+    //consulta si puede confundir aviones aliados con aviones enemigos considerando las placas de estos
     public boolean problemaEnAire(){
         return true;
     }
 
-    public ArrayList<Object> seranDestruidos(int longitud, int latitude){
-        return ArrayList<>;
+    // Devuelve la lista de barcos que serán destruidos en la posición (longitud, latitud)
+    public ArrayList<Barco> seranDestruidos(int longitud, int latitude){
+        return new ArrayList<Barco>();
     }
 }
